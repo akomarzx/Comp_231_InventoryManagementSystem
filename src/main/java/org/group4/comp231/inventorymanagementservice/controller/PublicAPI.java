cf55a52dc9cb28fa55e4084b40d342d92302c206
Ronald Jr Ombao <rombao@my.centennialcollege.ca>
2024-01-22 17:46:28 -0500

    Testing for initial security setup

--- src/main/java/org/group4/comp231/inventorymanagementservice/controller/PublicAPI.java
+++ src/main/java/org/group4/comp231/inventorymanagementservice/controller/PublicAPI.java
@@ -6,12 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// TODO: For testing purposes
 @RestController
 @RequestMapping("/public")
 public class PublicAPI {
 
-    @GetMapping
+    @GetMapping("/message")
     public ResponseEntity<String> getMessage() {
-        return new ResponseEntity<>("Hello World", HttpStatus.OK);
+        return new ResponseEntity<>("Hello World - Public", HttpStatus.OK);
+    }
+
+    @GetMapping("/message/messages")
+    public ResponseEntity<String> getMessage2() {
+        return new ResponseEntity<>("Hello World - Public 2", HttpStatus.OK);
+    }
+
+    @GetMapping("inventory")
+    public ResponseEntity<String> getMessage3() {
+        return new ResponseEntity<>("Hello World - Public 3 Inventory", HttpStatus.OK);
     }
 }
