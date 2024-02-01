a796d18cf80914e3e8d30474142cd4b826c0b974
Ronald Jr Ombao <rombao@my.centennialcollege.ca>
2024-01-31 20:35:05 -0500

    Tenant Feature WIP

---
+++ src/main/java/org/group4/comp231/inventorymanagementservice/controller/TenantController.java
@@ -0,0 +1,12 @@
+package org.group4.comp231.inventorymanagementservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tenant")
+public class TenantController {
+
+
+
+}
---
+++ src/main/java/org/group4/comp231/inventorymanagementservice/services/TenantService.java
@@ -0,0 +1,7 @@
+package org.group4.comp231.inventorymanagementservice.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TenantService {
+}
